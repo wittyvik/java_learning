9df0326f486984bb49481a8dac92317e962c603c
vjadhav <vikas.jadhav@finicity.com>
2024-08-16 16:04:33 +0530

    Rectified Changes

--- springboot_rest_mysql_app/src/main/java/in/mylearning/springboot_rest_mysql_app/SpringbootRestMysqlAppApplication.java
+++ springboot_rest_mysql_app/src/main/java/in/mylearning/springboot_rest_mysql_app/SpringbootRestMysqlAppApplication.java
@@ -2,8 +2,10 @@ package in.mylearning.springboot_rest_mysql_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringbootRestMysqlAppApplication {
 
 	public static void main(String[] args) {
--- springboot_rest_mysql_app/src/main/java/in/mylearning/springboot_rest_mysql_app/binding/Course.java
+++ springboot_rest_mysql_app/src/main/java/in/mylearning/springboot_rest_mysql_app/binding/Course.java
@@ -3,17 +3,27 @@ package in.mylearning.springboot_rest_mysql_app.binding;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 /**
  * @author Vikas Jadhav
  */
 
 @Data
 @Entity
+@Table(name = "course_data")
 public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Integer cid;
+
+    @Column(name = "course_name")
     private String name;
+
+    @Column(name = "course_price")
     private double price;
+
+    @Column(name = "enrollment_date")
+    private LocalDate enrollment_date;
 }
